2032c28c72b78c565298c1ed0f29908ba7e3ddea
AZen <418130444@qq.com>
2023-04-27 16:20:42 +0800

    update

--- 4.FlowControl/test.py
+++ 4.FlowControl/test.py
@@ -1,8 +1,7 @@
-
-
 for letter in 'Python':
     if letter == 'h':
-        pass 
-        print("这是pass块")
-    print("当前字母：", letter)
-print("Good bye!")
+        pass
+        print ('这是 pass 块')
+    print ('当前字母 :', letter)
+ 
+print ("Good bye!")
