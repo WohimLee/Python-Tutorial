27ac2ddf0364f7b6e474b9e9e37b345ea31a77c3
AZen <418130444@qq.com>
2023-05-01 15:35:37 +0800

    update

--- 11.Libraries/2.Matplotlib/test.py
+++ 11.Libraries/2.Matplotlib/test.py
@@ -1,9 +1,11 @@
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.array(["Test-1","Test-2","Test-3","Test-4"])
+
+x = np.array(["Test-1", "Test-2", "Test-3", "C-TEST"])
 y = np.array([12, 22, 6, 18])
 
-plt.barh(x, y, height = 0.1, color = ["#4CAF50","red","hotpink","#556B2F"])
-
-plt.savefig("./test.jpg", dpi=300)
+plt.barh(x, y,  color = ["#4CAF50","red","hotpink","#556B2F"], height=0.5)
+plt.savefig("./imgs/bar2.png")
