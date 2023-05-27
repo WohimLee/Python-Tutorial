644ef038cdca0f0899fa548ed930eb255d31c17b
AZen <418130444@qq.com>
2023-05-27 16:27:24 +0800

    update

--- 8.OOP/utils.py
+++ 8.OOP/utils.py
@@ -7,13 +7,15 @@ def mnist_labels(path):
     _, num_item = struct.unpack_from(">II", data, 0)
     labels = struct.unpack_from("B"*num_item, data, 8)
     return np.array(labels)
-    
+
+
 def mnist_images(path):
     with open(path, "rb") as f:
         data = f.read()
     _, num_item, rows, cols = struct.unpack_from(">iiii", data, 0)
     images = struct.unpack_from("B"*num_item*rows*cols, data, 16)
     return np.array(images).reshape(num_item, -1)
+
 
 def one_hot(labels, classes):
     '''
