f574c8b878d0fe27705436ea087437a02e9c8990
AZen <418130444@qq.com>
2023-11-27 15:53:27 +0800

    update

--- MultiProcessing/src/slam.py
+++ MultiProcessing/src/slam.py
@@ -5,17 +5,30 @@ import torch.multiprocessing as mp
 
 class SLAM:
     def __init__(self):
-        pass
+        self.only_track = False    
+    
+    def tracking(self, rank):
+        print("Running tracking...")
     
     
-    def tracking(self):
-        pass
-    
-    
-    def mapping(self):
-        pass
+    def mapping(self, rank, stop=False):
+        print("Running mapping...")
+        
     
     def run(self):
+        stop = True
         processes = [
-            mp.Process(target=self.tracking)
+            mp.Process(target=self.tracking, args=(0, )),
+            mp.Process(target=self.mapping, args=(1, stop if self.only_track else (not stop)))
         ]
+        
+        for p in processes:
+            p.start()
+            
+        for p in processes:
+            p.join()
+        
+        
+if __name__ == '__main__':
+    slam = SLAM()
+    slam.run()
